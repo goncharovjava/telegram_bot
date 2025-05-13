515cfea26d7bf29b967d1d5ed7a174b046112a74
”Goncharov <goncharovjava@gmail.com>
2025-05-13 23:08:07 +0700

    t_b-3 added Command pattern for handling Telegram Bot commands

---
+++ src/main/java/com/github/javarushcommunity/jrtb/command/StartCommand.java
@@ -0,0 +1,29 @@
+package com.github.javarushcommunity.jrtb.command;
+
+import com.github.javarushcommunity.jrtb.service.SendBotMessageService;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+/**
+ * Start {@link Command}.
+ */
+public class StartCommand implements Command{
+
+    private final SendBotMessageService sendBotMessageService;
+
+    public final static String START_MESSAGE = "Привет я телеграм бот, чем я могу быть полезен?";
+
+
+
+    // Здесь не добавляем сервис через получение из Application Context.
+    // Потому что если это сделать так, то будет циклическая зависимость, которая
+    // ломает работу приложения.
+
+    public StartCommand(SendBotMessageService sendBotMessageService) {
+        this.sendBotMessageService = sendBotMessageService;
+    }
+
+    @Override
+    public void execute(Update update) {
+        sendBotMessageService.sendMessage(update.getMessage().getChatId().toString(), START_MESSAGE);
+    }
+}
---
+++ src/test/java/UnknownCommandTest.java
@@ -0,0 +1,23 @@
+import com.github.javarushcommunity.jrtb.command.Command;
+import com.github.javarushcommunity.jrtb.command.UnknownCommand;
+import org.junit.jupiter.api.DisplayName;
+
+import static com.github.javarushcommunity.jrtb.command.UnknownCommand.UNKNOWN_MESSAGE;
+
+@DisplayName("Unit-level testing for UnknownCommand")
+public class UnknownCommandTest extends AbstractCommandTest{
+    @Override
+    String getCommandName() {
+        return "/fdgdfgdgfdbd";
+    }
+
+    @Override
+    String getCommandMessage() {
+        return UNKNOWN_MESSAGE;
+    }
+
+    @Override
+    Command getCommand() {
+        return new UnknownCommand(sendBotMessageService);
+    }
+}
